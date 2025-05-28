1c008444391d00fc9e7b2294e79f2ca6bd809b8d
Ing3rulDrogat <myta.boss39@yahoo.com>
2025-05-29 00:21:52 +0100

    Fixed positioning on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,23 +7,22 @@ const galadaFont = Galada({
 });
 export default function Home() {
   return (
-    <div className="w-full h-full">
-      <div className="bg-[url('/TempBG.png')]  bg-fixed min-h-screen bg-repeat bg-top bg-contain">
-        <div className="backdrop-brightness-50 min-h-screen">
-          <div className=" justify-items-center">
-            <div className="bg-[url('/BG.png')] bg-center bg-no-repeat w-full h-screen bg-amber-700 mb-50">
-              <Header />
-              <div className="w-full h-4/12 content-center justify-items-center pb-10">
+    <div className="w-full h-full relative">
+      <div className="bg-[url('/TempBG.png')]  bg-fixed  bg-repeat bg-top bg-contain">
+        <div className="backdrop-brightness-50 flex flex-col">
+          <div className=" justify-items-center ">
+            <Header />
+            <div className="bg-[url('/BG.png')] bg-center bg-no-repeat w-full h-screen bg-amber-700 mb-50 -z-10 absolute top-0">
+              <div className="w-full h-4/12 content-center justify-items-center pt-50">
                 <p className={`text-6xl ${galadaFont.className}`}>Welcome to The Iron Flame Steakhouse</p>
                 <p className={`text-4xl ${galadaFont.className}`}>
                   Where Fire Meets Flavor — Indulge in the <b className="text-amber-400">Finest</b> Cuts of Meat
                 </p>
               </div>
             </div>
+            <div className="w-full h-screen" /> {/*Due to absolute positioning, have to have this here to render rest of the page properly */}
             <div className="w-400 h-50 bg-amber-700 mb-50 "></div>
-
             <div className="w-400 h-50 bg-amber-700 mb-50"></div>
-
             <div className="w-400 h-50 bg-amber-700"></div>
           </div>
         </div>
