b010472329e768cab9693a9e80caf51215348c82
Ing3rulDrogat <myta.boss39@yahoo.com>
2025-05-29 19:39:15 +0100

    Fixed issues with sidebar and burger icon animations

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,22 @@ import Link from "next/link";
 
 function Header() {
   const [showSideBar, setshowSideBar] = useState(false);
+  const [animationType0, setanimationType0] = useState("");
+  const [animationType1, setanimationType1] = useState("");
+  const [animationType2, setanimationType2] = useState("");
 
   const openSideBar = () => {
     console.log("Works so far");
     setshowSideBar(!showSideBar);
+    if (animationType0 == "animate-topBurgerIcon") {
+      setanimationType0("animate-topBurgerIconRe");
+      setanimationType1("animate-middleBurgerIconRe");
+      setanimationType2("animate-bottomBurgerIconRe");
+    } else {
+      setanimationType0("animate-topBurgerIcon");
+      setanimationType1("animate-middleBurgerIcon");
+      setanimationType2("animate-bottomBurgerIcon");
+    }
   };
 
   return (
@@ -16,9 +28,9 @@ function Header() {
       <div className="sticky top-0 w-full h-35 bg-gradient-to-t  from-opacity to-black flex justify-between items-center animate-easeInTop">
         <div className="flex w-3/12 h-full items-center">
           <div className=" w-4/12 h-full content-center justify-items-center cursor-pointer" onClick={openSideBar}>
-            <div className="bg-white w-4/12 h-1/32 rounded-4xl my-2"></div>
-            <div className="bg-white w-4/12 h-1/32 rounded-4xl my-2"></div>
-            <div className="bg-white w-4/12 h-1/32 rounded-4xl my-2"></div>
+            <div className={`bg-white w-12 h-1 rounded-4xl my-2 ${animationType0}`}></div>
+            <div className={`bg-white w-12 h-1 rounded-4xl my-2 ${animationType1}`}></div>
+            <div className={`bg-white w-12 h-1 rounded-4xl my-2 ${animationType2}`}></div>
           </div>
           <Link href={"/menu"}>Menu</Link>
         </div>
@@ -34,6 +46,7 @@ function Header() {
         </div>
       ) : (
         <div className=" w-full h-screen  sticky top-35">
+          {/* <div className="bg-red-300 w-1/4 h-11/12 cursor-pointer hidden animate-easeInLeft" style={{ animationDirection: "reverse" }}></div> */}
           <div className="bg-red-300 w-1/4 h-11/12 cursor-pointer hidden animate-easeOutLeft"></div>
         </div>
       )}
