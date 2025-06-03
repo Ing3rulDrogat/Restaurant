ec1cf96ea8a0bb77531014b612ff43b2f3ef7c24
IlieRobertUrsu <dobicanrobert@gmail.com>
2025-06-03 21:27:41 +0100

    work on signup / login pgs

--- src/app/registration/page.tsx
+++ src/app/registration/page.tsx
@@ -1,8 +1,17 @@
 "use client";
+import Header from "@/components/Header";
 import React, { useState } from "react";
+
+import { CiLogin } from "react-icons/ci";
+
+import { FaUserPlus } from "react-icons/fa";
+
+
 
 function LogIn() {
   const [animation, setanimation] = useState("");
+
+
 
   const animateBar = () => {
     if (animation !== "animate-BarRight") {
@@ -13,28 +22,87 @@ function LogIn() {
   };
   const function2 = () => {};
 
+
+
   return (
-    <div className="w-screen h-screen content-center justify-items-center">
-      <div className="w-1/2 h-1/2 bg-amber-700 flex flex-col items-center">
+    <div className="w-screen h-screen content-center justify-items-center bg-gradient-to-t from-black from-5% to-gray-600">
+      <Header></Header>
+      <div className="w-1/5  h-2/3 bg-white/55 flex flex-col items-center rounded-3xl shadow-lg shadow-cyan-700/10 ">
         <div className="w-full flex flex-col  items-center">
-          <ul className="flex justify-center">
+          <ul className="flex justify-center">  
             <li className="flex mx-3 text-3xl  cursor-pointer" onClick={animateBar}>
-              Log-In
+              Log In 
             </li>
             <li className="mx-3 text-3xl cursor-pointer " onClick={animateBar}>
-              Sign-Up
+              Sign Up
             </li>
           </ul>
-          <div className="bg-black w-1/3 h-1  overflow-hidden">
-            <div className={`bg-green-500 w-1/2 h-full ${animation}`}></div>
+          <div className="bg-black w-1/2  h-1  overflow-hidden">
+            <div className={`bg-cyan-500 w-1/2 h-full ${animation}`}></div>
           </div>
         </div>
-        <div className="bg-green-300 w-2/3 h-2/3 my-10">
-          {/* Log-In */}
-          <div></div>
-          {/* Sign Up */}
-          <div></div>
-        </div>
+        {animation !== "animate-BarRight" ? (
+          <div className="w-2/3 h-2/3 my-10">
+            {/* Log-In */}
+            <div className="flex flex-col items-center">
+              <input
+                type="email"
+                placeholder="Email Address"
+                className="mb-4 p-2 border border-black rounded text-black placeholder-black"
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                className="mb-4 p-2 border border-black rounded text-black placeholder-black"
+              />
+              <button
+                onClick={function2}
+                className="flex items-center bg-amber-500 text-black px-4 py-2 rounded hover:bg-cyan-600"
+              >
+                  Log In  <CiLogin className="mx-2  text-2xl" />
+              </button> 
+            </div>
+          </div>
+        ) : (
+          <div className="w-2/3 h-2/3 my-10">
+            {/* Sign-Up */}
+            <div className="flex flex-col items-center">
+              <input
+                type="Email"
+                placeholder="Email Address"
+                className="mb-4 p-2 border border-black rounded text-black placeholder-black"
+              />
+              <input
+                type="Address"
+                placeholder="Address"
+                className="mb-4 p-2 border border-black rounded text-black placeholder-black"
+              />
+              <input
+                type="Postal code"
+                placeholder="Postal code"
+                className="mb-4 p-2 border border-black rounded text-black placeholder-black"
+              />
+              <input
+                type="phone number"
+                placeholder="Phone Number"
+                className="mb-4 p-2 border border-black rounded text-black placeholder-black"
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                className="mb-4 p-2 border border-black rounded text-black placeholder-black"
+              />
+              <button
+                onClick={function2}
+              
+                className="flex items-center bg-amber-500 text-black px-2 py-2 rounded hover:bg-cyan-600"
+              >
+                  Sign Up  <FaUserPlus className="mx-2  text-1xl" />
+
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
